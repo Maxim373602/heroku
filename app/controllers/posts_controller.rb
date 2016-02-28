fdb1983da54aa6b727229880887efedb52df375c
Maxim373602 <maxim.pristupa@hotmail.com>
2016-02-29 02:02:21 +0300

    heroku

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except:[:index]
+
+  def index
+  	@posts=Post.all.order("created_at DESC")
+  end
+  
+  def new
+
+  end
+
+  def create 
+  	@post=Post.new(post_params)
+  	if @post.save
+  		redirect_to @post
+  	else
+  		render 'new'
+  	end
+  end
+
+  def show
+  	@post=Post.find(params[:id])
+  end
+
+private
+  def post_params
+		params.require(:post).permit(:title, :content, :image)
+  end	
+end
